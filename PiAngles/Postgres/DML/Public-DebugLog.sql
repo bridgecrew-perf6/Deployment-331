db4ffbb9355c9ab3ec3103d634b56f8a1a2dec4e
saradhivs <saradhivs@yahoo.com>
2020-12-27 17:52:28 -0500

    Initial DML upload.

---
+++ PiAngles/Postgres/DML/Public-DebugLog.sql
@@ -0,0 +1,9 @@
+--http://stackoverflow.com/questions/273437/how-do-you-debug-mysql-stored-procedures
+DROP PROCEDURE IF EXISTS public.DebugLog;
+
+CREATE PROCEDURE public.DebugLog(msg VARCHAR(255))
+AS $$
+BEGIN
+	select concat("** ", msg) AS '** DEBUG:';
+END
+$$ LANGUAGE plpgsql;
