cb05a01702d7a67807fdde063b86189f3dd7c77a
saradhivs <saradhivs@yahoo.com>
2020-12-28 07:47:52 -0500

    Continuing reorganising.

--- PiAngles/Scripts/dockerBuildAndPush.sh
+++ PiAngles/Scripts/dockerBuildAndPush.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd ../Services/
+
+docker-compose build
+
 #docker images | grep pianglesorg | sed 's/|/ /' | awk '{print $1, $3}'
 docker images | grep pianglesorg | sed 's/|/ /' | while read -r line ; do
 	CMD=`echo $line | awk '{print "docker tag " $3 " "  $1"; docker push "$1}'`
